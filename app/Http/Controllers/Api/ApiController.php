f0fb64a9098c1f3e7f1179cce04ee92c6f5e82fc
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-07-26 22:27:29 +0700

    Add DB (query builder)

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Artisan;
@@ -19,7 +20,7 @@ class ApiController extends Controller
     }
     public function product($category)
     {
-        $products = Product::where('category_id', '=', "$category")->get();
+        $products = Product::where('category_id', '=', "$category")->first();
         $data['data'] = $products;
         return response()->json(['message'=>'success','data'=>$data]);
     }
@@ -33,29 +34,32 @@ class ApiController extends Controller
         $data['data'] = $categories;
         return response()->json(['message' => 'success', 'data' => $data]);
     }
-    public function transfer(Request $request, $id){
-        $credentials = $request->validate([
-            'email' => 'required',
-            'password' => 'required'
-        ]);
+    public function transfer(Request $request){
         $update_data = $request->validate([
-            'saldo' => 'required'
+            'from' => 'required',
+            'to' => 'required',
+            'amount' => 'required',
+            'api_key' => 'required'
         ]);
-        if ($credentials['email']=='admin@vixiloc.my.id'){
-            if (Auth::attempt($credentials)){
-                $user = User::find($id)->first();
-                $user->saldo=$user['saldo']+$update_data['saldo'];
-                $user->save();
-                $response['response']=[
-                    'id'=>$user['id'],
-                    'email'=>$user['email'],
-                    'saldo'=>$user['saldo']
-                ];
-                return response()->json(['message' => 'success', 'data' => $response]);
+        $sender = DB::table('users')->where('email','=',$update_data['from'])->first();
+        $user = DB::table('users')->where('email', '=', $update_data['to'])->first();        
+        $saldo_pengirim = $sender->saldo;
+        $saldo_penerima = $user->saldo;
+        $req_api = $update_data['api_key'];
+        $api_key = $sender->api_key;
+        $jumlah_transfer = $update_data['amount'];
+        $kurangi_saldo = $saldo_pengirim-$jumlah_transfer;
+        $tambah_saldo = $saldo_penerima+$jumlah_transfer;
+        if ($req_api==$api_key) {
+            if ($saldo_pengirim>=$jumlah_transfer) {
+                DB::table('users')->where('email', '=', $update_data['from'])->update(['saldo'=>$kurangi_saldo]);
+                DB::table('users')->where('email', '=', $update_data['to'])->update(['saldo' => $tambah_saldo]);
+                return response()->json(['message' => 'Berhasil transfer saldo anda telah di kirim']);
+            } else {
+                return response()->json(['message'=>'Gagal transfer uang anda tidak cukup']);
             }
-            else {
-                return response()->json(['message'=>'failed','data'=>'']);
-            }
+        }else {
+            return response()->json(['message'=>'API key anda salah silahkan cek ulang dan ulangi request']);
         }
     }
     public function route_cache(){
